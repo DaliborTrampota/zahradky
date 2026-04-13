823e0d7770715b06e4bac455724df03c3f3e64b3
Dalibor Trampota <dalibor.trampota@autodesk.com>
2026-04-13 17:38:33 +0200

    fix invite redirect supabase

--- src/index.jsx
+++ src/index.jsx
@@ -3,6 +3,11 @@ import { render } from 'solid-js/web'
 import './index.css'
 import App from './App.jsx'
 
+// Redirect invite tokens to /invite before the app mounts
+if (window.location.hash.includes('type=invite') && window.location.pathname === '/') {
+  window.location.replace('/invite' + window.location.hash)
+}
+
 const root = document.getElementById('root')
 
 render(() => <App />, root)
